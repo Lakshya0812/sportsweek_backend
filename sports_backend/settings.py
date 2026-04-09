bcab3438ad0f075dc9d1f432b548874af62db05d
Lakshya Gurbani <lakshya.gurbani@stitchmes.com>
2026-04-09 10:42:28 +0530

    Add Vercel URL to CORS allowed origins

--- sports_backend/settings.py
+++ sports_backend/settings.py
@@ -134,7 +134,7 @@ SIMPLE_JWT = {
 # CORS
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:5173,http://localhost:3000'
+    default='http://localhost:5173,http://localhost:3000,https://sportsweek-frontend.vercel.app'
 ).split(',')
 CORS_ALLOW_CREDENTIALS = True
 
